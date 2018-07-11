2875e9cf25218cac2c8b76894ed20a9fbd0e63c4
mmorinie <maxime.moriniere@hotmail.fr>
2018-07-11 18:15:07 -0400

    Update globals.py

--- mybigdft/globals.py
+++ mybigdft/globals.py
@@ -5,18 +5,15 @@ import yaml
 # Read the definition of the input variables from the BigDFT sources
 try:
     BIGDFT_SOURCES = os.environ["BIGDFT_SOURCES"]
+    inp_vars_file = os.path.join(BIGDFT_SOURCES,
+                                 "src/input_variables_definition.yaml")
+    with open(inp_vars_file, "r") as f:
+        source = yaml.load_all(f)
+        inp_vars = next(source)
+        profiles = next(source)
 except KeyError:
-    pass
-    warnings.warn("BigDFT sources not found. Default files will be used "
-                  "instead (might lead to unwanted errors)", RuntimeWarning)
-    BIGDFT_SOURCES = os.path.normpath(
-        os.path.join(os.path.abspath(__file__), ".."))
-inp_vars_file = os.path.join(BIGDFT_SOURCES,
-                             "src/input_variables_definition.yaml")
-with open(inp_vars_file, "r") as f:
-    source = yaml.load_all(f)
-    inp_vars = next(source)
-    profiles = next(source)
+    inp_vars = {}
+    profiles = {}
 # Add the posinp key (as it is not in input_variables_definition.yaml)
 inp_vars["posinp"] = {"units": {"default": "atomic"},
                       "cell": {"default": []},
