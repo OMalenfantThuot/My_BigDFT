f789656816a3c95edbfa361ac1a911befe225e65
mmorinie <maxime.moriniere@hotmail.fr>
2018-07-11 18:06:53 -0400

    Update globals.py

--- mybigdft/globals.py
+++ mybigdft/globals.py
@@ -5,14 +5,13 @@ import yaml
 # Read the definition of the input variables from the BigDFT sources
 try:
     BIGDFT_SOURCES = os.environ["BIGDFT_SOURCES"]
-    inp_vars_file = os.path.join(BIGDFT_SOURCES,
-                                 "src/input_variables_definition.yaml")
 except KeyError:
     warnings.warn("BigDFT sources not found. Default files will be used "
                   "instead (might lead to unwanted errors)", RuntimeWarning)
-    curdir = os.path.normpath(os.path.join(os.path.abspath(__file__), ".."))
-    BIGDFT_SOURCES = os.path.join(curdir,
-                                  "src/input_variables_definition.yaml")
+    BIGDFT_SOURCES = os.path.normpath(
+        os.path.join(os.path.abspath(__file__), ".."))
+inp_vars_file = os.path.join(BIGDFT_SOURCES,
+                             "src/input_variables_definition.yaml")
 with open(inp_vars_file, "r") as f:
     source = yaml.load_all(f)
     inp_vars = next(source)
