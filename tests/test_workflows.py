5b871c8e560b424a5cc813162855013545dc80d4
mmorinie <maxime.moriniere@hotmail.fr>
2018-07-31 14:57:50 -0400

    Modified the input position of a test

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -78,10 +78,11 @@ class TestRamanSpectrum:
     ph = Phonons(gs)
 
     def test_run(self):
-        N2_ref = """2  angstroem
+        N2_ref = """\
+2   angstroem
 free
-N    2.976307744763e-23    6.872205902435e-23    1.071620018790e-02
-N   -1.104344885754e-23   -4.873421785298e-23    1.104273795769e+00"""
+N   3.571946174   3.571946174   3.620526682
+N   3.571946174   3.571946174   4.71401439"""
         ref_pos = Posinp.from_string(N2_ref)
         gs = Job(posinp=ref_pos, name='N2', run_dir='tests/phonons_N2')
         phonons = Phonons(gs)
@@ -89,14 +90,14 @@ N   -1.104344885754e-23   -4.873421785298e-23    1.104273795769e+00"""
         raman.run(nmpi=2, nomp=2)
         # Test the only physically relevant phonon energy
         np.testing.assert_almost_equal(
-            max(raman.energies['cm^-1']), 2.38632021e+03, decimal=6)
+            max(raman.energies['cm^-1']), 2386.9463343478246, decimal=6)
         # Test the only physically relevant intensity
         np.testing.assert_almost_equal(
-            max(raman.intensities), 22.561807427612163)
+            max(raman.intensities), 22.564457304830206)
         # Test the only physically relevant depolarization ratio
         i = np.argmax(raman.energies['cm^-1'])
         np.testing.assert_almost_equal(
-            raman.depolarization_ratios[i], 0.093748669614628299)
+            raman.depolarization_ratios[i], 0.09412173797731693)
         # Test that running the workflow again warns a UserWarning
         with pytest.warns(UserWarning):
             phonons.run()
