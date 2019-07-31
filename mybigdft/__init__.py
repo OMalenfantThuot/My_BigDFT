f07ebce8b643cca48c6360bb0ed6158543899dfe
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-07-31 15:34:30 -0400

    jobschnet added, nothing is working yet

--- mybigdft/__init__.py
+++ mybigdft/__init__.py
@@ -8,3 +8,4 @@ from __future__ import absolute_import
 from mybigdft.iofiles import InputParams, Posinp, Logfile, Atom
 from mybigdft.job import Job
 from mybigdft.workflows.workflow import Workflow
+from mybigdft.jobschnet import JobSchnet
---
+++ mybigdft/jobschnet.py
@@ -0,0 +1,21 @@
+"""
+The :class: `JobSchnet` class is the base class defining a SchnetPack calculation.
+"""
+
+from __future__ import print_function, absolute_import
+import os
+
+
+class JobSchnet(object):
+    r"""
+    This class defines a SchnetPack calculation similarly
+    to the Job class for BigDFT.
+    """
+
+    def __init__(self, name="", posinp=None, run_dir=None, model_dir=None, skip=False):
+        r"""
+        Parameters
+        ----------
+        name : str
+        """
+        pass
